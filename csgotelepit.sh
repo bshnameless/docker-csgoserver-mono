57fc70c3d142fd7099723c187218e73a218122ea
BlackStation - Mr.Nameless <hgtsoul@gmail.com>
2019-11-17 23:15:05 +0100

    Update csgotelepit.sh

--- csgotelepit.sh
+++ csgotelepit.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 cd /home/container
+sleep 1
+# Make internal Docker IP address available to processes.
+export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 
-apt install -y Xvfb wine-stable winbind
+# Update Source Server
+if [ ! -z ${SRCDS_APPID} ]; then
+    if [ ! -z ${SRCDS_BETAID} ]; then
+        if [ ! -z ${SRCDS_BETAPASS} ]; then
+            ./steamcmd/steamcmd.sh +login anonymous +force_install_dir /home/container +app_update ${SRCDS_APPID} -beta ${SRCDS_BETAID} -betapassword ${SRCDS_BETAPASS} +quit
+        else
+            ./steamcmd/steamcmd.sh +login anonymous +force_install_dir /home/container +app_update ${SRCDS_APPID} -beta ${SRCDS_BETAID} +quit
+        fi
+    else
+        ./steamcmd/steamcmd.sh +login anonymous +force_install_dir /home/container +app_update ${SRCDS_APPID} +quit
+    fi
+fi
 
-curl -sSL -o steamcmd.tar.gz http://media.steampowered.com/installer/steamcmd_linux.tar.gz
+# Replace Startup Variables
+MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+echo ":/home/container$ ${MODIFIED_STARTUP}"
 
-mkdir -p /srv/csgoserver/steamcmd
-tar -xzvf steamcmd.tar.gz -C /srv/csgoserver/steamcmd
-
-# SteamCMD fails otherwise for some reason, even running as root.\
-# This is changed at the end of the install process anyways.
-chown -R root:root /srv/csgoserver
-
-export HOME=/srv/csgoserver
-cd /srv/csgoserver
-export HOME=/srv/csgoserver
+# Run the Server
+eval ${MODIFIED_STARTUP}
