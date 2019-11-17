900df93cab056a80df063c8afd4d2f38bfffc90a
BlackStation - Mr.Nameless <hgtsoul@gmail.com>
2019-11-17 23:06:11 +0100

    Update csgotelepit.sh

--- csgotelepit.sh
+++ csgotelepit.sh
@@ -6,7 +6,7 @@ apt install -y Xvfb wine-stable winbind
 curl -sSL -o steamcmd.tar.gz http://media.steampowered.com/installer/steamcmd_linux.tar.gz
 
 mkdir -p /srv/csgoserver/steamcmd
-tar -xzvf steamcmd.tar.gz -C /srv/csgoserver
+tar -xzvf steamcmd.tar.gz -C /srv/csgoserver/steamcmd
 
 # SteamCMD fails otherwise for some reason, even running as root.\
 # This is changed at the end of the install process anyways.
@@ -14,5 +14,4 @@ chown -R root:root /srv/csgoserver
 
 export HOME=/srv/csgoserver
 cd /srv/csgoserver
-./steamcmd.sh +@sSteamCmdForcePlatformType windows +login anonymous +force_install_dir /srv/csgoserver +app_update 740 validate +quit
 export HOME=/srv/csgoserver
